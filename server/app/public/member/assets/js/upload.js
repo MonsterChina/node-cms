7cc991bdb0fd6feb1a731575ae9f4285aade3fc6
明空 <867528315@qq.com>
2023-10-29 23:13:39 +0800

    [功能新增] 后台移动端适配

--- server/app/public/member/assets/js/upload.js
+++
@@ -1,1 +0,0 @@
-import{h as a,A as t}from"./index.js";let o=o=>(console.log("data",o),a({url:"".concat(t.BASE_API,"/api/upload"),method:"post",headers:{"Content-type":"multipart/form-data"},data:o}));export{o as u};

65d12b46c79398c349e6e8e6bf8bed14c30d3c82
明空 <867528315@qq.com>
2023-11-05 21:20:17 +0800

    [样式优化] 2.0

---
+++ client/admin/src/api/upload.js
@@ -0,0 +1,19 @@
+import http from "../utils/http.js";
+import API from "../config/index.js";
+
+//上传
+// let formData = new FormData();
+// formData.append('file', file);
+// formData.append('fileName', file.name);
+
+export let upload = (data) => {
+  console.log("data", data);
+  return http({
+    url: `${API.BASE_API}/api/upload`,
+    method: "post",
+    headers: {
+      "Content-type": "multipart/form-data",
+    },
+    data: data,
+  });
+};

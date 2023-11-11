433d8bf22d0fe1411730cd495a741a80cc1fd003
明空 <867528315@qq.com>
2023-11-11 21:52:09 +0800

    [版本封板] 禅悦cms v2.0.2封板

--- server/app/config/config.base.js
+++ server/app/config/config.base.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const config = {};
-config.version = "v2.0.1";
+config.version = "v2.0.2";
 config.appName = "禅悦cms";
 config.port = '81';
-config.versionTime = "2023-11-9";
+config.versionTime = "2023-11-11";
 config.author = "明空";
 
 config.appRoot = path.join(__dirname, "../");

29593097e24e0376d3cf03c9703376d84e09c18d
明空 <867528315@qq.com>
2023-11-27 00:05:18 +0800

    [版本封板] 禅悦cms v2.0.3版本发布

--- server/app/config/config.base.js
+++ server/app/config/config.base.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 
 const config = {};
-config.version = "v2.0.2";
+config.version = "v2.0.3";
 config.appName = "禅悦cms";
 config.port = '81';
-config.versionTime = "2023-11-25";
+config.versionTime = "2023-11-26";
 config.author = "明空";
 
 config.appRoot = path.join(__dirname, "../");
